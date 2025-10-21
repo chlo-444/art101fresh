527c2d22651006924e585a487d19b488f3e2ff30
chlo-444 <cparsley@ucsc.edu>
2025-10-20 22:54:32 -0700

    env03

---
+++ exercises/env03.js
@@ -0,0 +1,32 @@
+
+// declaring an array with name myCommutes
+
+let myBugs = [" beetles, ", " flies, ", " ants, ", " and spiders. "];
+// declaring an object with name myFavouriteCommute
+
+let myFavouriteBugs = {
+    type: " green",
+    how: " snapy ",
+    look: " fuzzy.",
+
+    kind: ["crunchy, big, and sticky"],
+};
+
+let megaSentence;
+megaSentence = "<h2> Venus Fly Traps love to eat: " + myBugs[0] + myBugs[1] + myBugs[2] + myBugs[3] + "</h2>";
+
+megaSentence = megaSentence + "<h3>  They are " + myFavouriteBugs.type + ", " + myFavouriteBugs.how + ", and " + myFavouriteBugs.look + "</h3>";
+
+$("#output").html(megaSentence);
+
+//lets add a button to declare how many flies have been eaten
+
+
+let count = 0;
+
+$("#declare").click(function () {
+
+    count = count + 1;
+    $("#declare").html(" Dead Fly Count: " + count);
+});
+
