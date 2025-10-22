fe75c1c200ce40b9c84f75a57dd6a38e0fcd9b6b
chlo-444 <cparsley@ucsc.edu>
2025-10-21 18:50:14 -0700

    mywork2

--- exercises/ex04.js
+++ exercises/ex04.js
@@ -1,22 +1,20 @@
-let totalNumber = 0;
-
 let count = 0;
-let colors = ["Orchid", "Coral", "HotPink", "Plum"];//new line to add
-
-
+let colorIndex = 0;
+let mood;
+let colors = ["Orchid", "Coral", "HotPink", "Plum"];
 $("#needy-button").click(function () {
-
-    let reminder = count % colors.length;
-
-
-    $("#needy-button").html("Clicks: " + count + "color:" + colors[count]);
-    //new line to add
-
-    $("body").css("background-color", colors[reminder]);
-    count = count + 1
+   if (count < 5) { mood = "fresh and happy"; }
+   else if ((count >= 5) && (count < 10)) { mood = "keep pushing"; }
+   else { mood = "so tired!"; }
+   $("#needy-button").html("Clicks: " + count + " Color: " + colors[colorIndex] + " - " + mood);
 
 
+   $("body").css("background-color", colors[colorIndex]);
+   count = count + 1;
+   colorIndex = colorIndex + 1;
+   if (colorIndex == 4) { colorIndex = 0; } 
 });
+
 
 // once it is clicked
 // add one to the total number
---
+++ exercises/lab04.js
@@ -0,0 +1,20 @@
+
+// declaring an array with name myCommutes
+
+let myCommutes = ["metro bus", "UCSC taps bus", "friends", "walking"];
+// declaring an object with name myFavouriteCommute
+
+let myFavouriteCommute = {
+    type: "bus",
+    route: 18,
+    print: "butterfly",
+    hasMiddleDoor: true,
+    drivers: ["Malia, Michael, and Trader Joes Guy"],
+};
+let megaSentence;
+megaSentence = "<p> My top two communtes from the array are: " + myCommutes[0] + myCommutes[1] + myCommutes[2] + myCommutes[3] + "</p>";
+
+megaSentence = megaSentence + "<p> My favorite commute possesses such characteristics: type-" + myFavouriteCommute.type + ", route number " + myFavouriteCommute.route + ", the best driver:" + myFavouriteCommute.drivers[0] + "</p>";
+
+$("#output").html(megaSentence);
+
